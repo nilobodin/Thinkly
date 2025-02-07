c2e14aa211d0a1369a61a1c4f4a55f8e75697608
nilobodin <nikita.lobodin2005@mail.ru>
2025-02-08 01:41:43 +0600

    created a header and aside model, the main directors

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php include 'vendor/components/header.php'?>
+    <div class="container">
+        <div class="main-container">
+            <aside class="aside-nav">
+                <nav class="nav">
+                    <div class="nav_up">
+                        <a href="" class="nav-link">
+                            <div class="nav-item">
+                                <div class="nav-item--icon"><img src="/assets/img/icons/home-active.svg" alt=""></div>
+                                <div class="nav-item--text active-nav">
+                                    <p>Домашняя страница</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="" class="nav-link">
+                            <div class="nav-item">
+                                <div class="nav-item--icon"><img src="/assets/img/icons/quest.svg" alt=""></div>
+                                <div class="nav-item--text">
+                                    <p>Вопросы</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="nav_down">
+                        <a href="" class="nav-link">
+                            <div class="nav-item">
+                                <div class="nav-item--icon"><img src="/assets/img/icons/user.svg" alt=""></div>
+                                <div class="nav-item--text">
+                                    <p>Профиль</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="" class="nav-link">
+                            <div class="nav-item">
+                                <div class="nav-item--icon"><img src="/assets/img/icons/trofy.svg" alt=""></div>
+                                <div class="nav-item--text">
+                                    <p>Награды</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="" class="nav-link">
+                            <div class="nav-item">
+                                <div class="nav-item--icon"><img src="/assets/img/icons/users.svg" alt=""></div>
+                                <div class="nav-item--text">
+                                    <p>Пользователи</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </nav>
+            </aside>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ vendor/components/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/fonts.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Thinkly</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="header_row-container">
+                <div class="logo">
+                    <img src="assets/img/logo.png" alt="">
+                </div>
+                <div class="search">
+                    <div class="search-icon">
+                        <img src="/assets/img/icons/search-loupe.svg" alt="" class="search-icon-svg">
+                    </div>
+                    <input type="text" placeholder="Начните вводить свой вопрос...">
+                </div>
+                <div class="header-btns">
+                    <input type="submit" value="Войти" class="btn_enter">
+                    <div class="btn_theme"><img src="/assets/img/icons/sun.svg"></div>
+                </div>
+            </div>
+        </div>
+    </header>
